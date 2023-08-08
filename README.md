fb36d40a686cf086cb6641034f5a3ad4f1b70a4e
Pablo Klaschka <contact@pabloklaschka.de>
2023-08-08 03:14:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+![Pride Flagger Plugin Logo](logos/marketing.svg)
+
+# Pride Flagger for Adobe InDesign
+
+This is a UXP plugin for Adobe InDesign that allows you to add pride flags to your documents. It was created by [Zuri Klaschka (they/them)](https://github.com/pklaschka).
+
+![Rainbow Pride Flag](img/rainbow.svg)
+![Trans Pride Flag](img/trans.svg)
+![Non-binary Pride Flag](img/non-binary.svg)
+
+## Installation
+
+### From the Adobe Exchange
+
+(Coming soon)
+
+### From the Releases
+
+1. Download the latest release from the [releases page](https://github.com/pklaschka/indesign-pride-flagger/releases).
+2. Double-click the `.ccx` file to install it.
+3. Follow the instructions in the installer.
+4. Restart Adobe InDesign.
+
+### From Source
+
+1. Clone this repository.
+2. Run the UXP Developer Tool (installed with Adobe Creative Cloud Desktop).
+3. Click "Add Plugin" and select the `manifest.json` file in the repository.
+4. Run InDesign (v18.5 or above).
+5. In the UXP Developer Tool, click "Load" und the Pride Flagger plugin's menu.
+
+## Usage
+
+### Adding a Pride Flag page
+
+1. Open the Pride Flagger plugin from the "Plugins" menu.
+2. Make sure you haven't selected any objects in your document (in the Pride Flagger panel, there should be a blue bar saying _"Creating a new page (nothing selected)"_).
+3. Click the button corresponding to the flag you want to add.
+4. A new page will be added to your document with the flag on it.
+
+### Adding a Pride Flag to an existing page
+
+1. Open the Pride Flagger plugin from the "Plugins" menu.
+2. Select the object you want to add the flag to (you can only select rectangles and groups at the moment).
+3. Click the button corresponding to the flag you want to add.
+4. The flag will be added in the place of (and replacing) the selected frame.
+
+## FAQ
+
+### Why is the plugin called "Pride Flagger"?
+
+Because it adds pride flags to your document.
+
+### Should you use pride flags in commercial settings?
+
+Yes, you should. Pride flags are a symbol of acceptance and inclusion, and should be used in all settings, including commercial ones.
+
+However, you should make sure that you don't just add pride flags to your documents, but also make sure that you are actively supporting the LGBTQIA+ community. Otherwise, it might get seen as "rainbow-washing" (similar to "green-washing"), which the community doesn't usually appreciate.
+
+### Don't you also do a kind of rainbow washing by building this plugin and thus creating a product from it?
+
+No. First of all, I myself am part of the LGBTQIA+ community (to be more precise, of the 🏳️‍⚧️ _T_ and the _A_ in the acronym), so I'm not doing this for money. I'm not selling this plugin (and instead provide it as free and open-source software). I'm just building it because I think it's fun, and because I myself want to use it.
+
+### Why would you want to add pride flags to your documents?
+
+Because it's fun, and because it shows that you support the LGBTQIA+ community.
+
+### Why are there only a few flags?
+
+I'm adding more flags as I go. If you want to add a flag, feel free to open a pull request.
+
+### Is this the first (complete) open-source UXP-based plugin for Adobe InDesign ever published?
+
+I ... think so 😅.
---
+++ index.cjs
@@ -0,0 +1,358 @@
+const {
+	app,
+	VerticalJustification,
+	Justification,
+	ScriptLanguage,
+	UndoModes,
+	Rectangle,
+	Group,
+	Application, ColorSpace
+} = require("indesign");
+const { entrypoints } = require("uxp");
+
+// more to get type definitions for the InDesign DOM than anything else:
+if (!(app instanceof Application)) {
+	throw new Error('This script must be run from InDesign');
+}
+
+const cbPadding = document.querySelector('#cb-padding');
+const txtTextfield = document.querySelector('#txt-message');
+
+const buttons = {
+	'#btn-rainbow': createRainbowFlag,
+	'#btn-asexual': createAsexualFlag,
+	'#btn-aromantic': createAromanticFlag,
+	'#btn-bisexual': createBisexualFlag,
+	'#btn-pan': createPanFlag,
+	'#btn-lesbian': createLesbianFlag,
+	'#btn-gay-men': createGayMenFlag,
+	'#btn-trans': createTransFlag,
+	'#btn-non-binary': createNonBinaryFlag,
+	'#btn-genderqueer': createGenderqueerFlag,
+	'#btn-genderfluid': createGenderfluidFlag,
+	'#btn-agender': createAgenderFlag,
+}
+
+Object.entries(buttons).forEach(([selector, flagFunction]) => {
+	const button = document.querySelector(selector);
+	button.addEventListener('click', () =>
+		createPrideFlag(flagFunction, `${button.textContent.trim()}`)
+	);
+});
+
+/**
+ * @type {typeof EventListeners[number]}
+ */
+let listener = app.eventListeners.add('afterSelectionChanged', () => {
+	console.log(`Selection changed: ${app.selection.length} items selected`);
+	checkSelection();
+});
+
+entrypoints.setup({
+	plugin: {
+		async create() {
+			console.log(`Plugin create called`);
+		},
+		async destroy() {
+			console.log(`Plugin destroy called`);
+			if (listener.isValid)
+				listener.remove();
+		}
+	}
+})
+
+
+function createPrideFlag(flagFunction, historyName = 'Add pride flag') {
+	if (checkSelection() === 'invalid')
+		return window.alert('Please select a rectangle or group to replace, or nothing to create a new page');
+	app.doScript(() => {
+		const newPage = getOrCreateFlagLocation();
+		flagFunction(newPage);
+		if (getTextMessage() !== undefined) {
+			createText(newPage);
+		}
+	}, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT, historyName);
+}
+
+function getTextMessage() {
+	if (!txtTextfield || txtTextfield.value === '') {
+		return undefined;
+	}
+	return txtTextfield.value.toString();
+}
+
+/**
+ *
+ * @param {Page} parent
+ * @param bounds
+ * @param color
+ * @param index
+ * @param total
+ * @returns {*}
+ */
+function createStripe({ parent, bounds }, color, index = 0, total = 1) {
+	const rect = parent.rectangles.add();
+	rect.strokeWeight = 0;
+	rect.fillColor = color;
+	rect.geometricBounds = [
+		bounds[0] + (bounds[2] - bounds[0]) / total * index,
+		bounds[1],
+		bounds[0] + (bounds[2] - bounds[0]) / total * (index + 1),
+		bounds[3]
+	];
+
+	return rect;
+}
+
+function createStripes(flagLocation, colors) {
+	const total = colors.length;
+
+	const mapColorsToSprites = (color, index) => {
+		if (color === undefined) return;
+		return createStripe(flagLocation, color, index, total)
+	};
+
+	const stripes = colors.map(mapColorsToSprites).filter(Boolean);
+	const group = flagLocation.parent.groups.add(stripes);
+	app.selection = [group];
+	return group;
+}
+
+const alert = document.querySelector('#selection-status-alert');
+const mode = document.querySelector('#mode');
+
+checkSelection();
+function checkSelection() {
+	if (app.selection.length > 1) {
+		alert.classList.add('alert__invalid');
+		mode.textContent = 'Please select only one layer (or nothing)';
+
+		return 'invalid';
+	}
+	if (app.selection.length === 0) {
+		alert.classList.remove('alert__invalid');
+		mode.textContent = 'Creating a new page (nothing selected)';
+
+		return 'new';
+	}
+	const selectionElement = app.selection[0];
+	if (!(selectionElement instanceof Rectangle) && !(selectionElement instanceof Group)) {
+		alert.classList.add('alert__invalid');
+		mode.textContent = 'Please select a rectangle or group';
+
+		return 'invalid';
+	}
+
+	alert.classList.remove('alert__invalid');
+	mode.textContent = 'Replacing selection';
+	return 'replace';
+}
+
+
+/**
+ * Get the current selection's parent page or create a new page if there is no selection
+ * @returns {{parent: import('indesign').Page, bounds: [number, number, number, number]}}
+ */
+function getOrCreateFlagLocation() {
+	if (app.selection.length > 1) throw new Error('Please select only one layer');
+	if (app.selection.length === 0) {
+		const doc = app.activeDocument;
+		const newPage = doc.pages.add();
+		return { parent: newPage, bounds: newPage.bounds };
+	}
+	const selectionElement = app.selection[0];
+	if (!(selectionElement instanceof Rectangle) && !(selectionElement instanceof Group)) throw new Error('Please select a rectangle');
+
+	const flagLocation = { parent: selectionElement.parentPage, bounds: selectionElement.geometricBounds };
+	selectionElement.remove();
+	return flagLocation;
+}
+
+function createTransFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Trans Flag Blue', '#5BCFFB'),
+		getOrCreateColor('Trans Flag Pink', '#F5A9B8'),
+		getOrCreateColor('Trans Flag White', '#FFFFFF'),
+		getOrCreateColor('Trans Flag Pink', '#F5A9B8'),
+		getOrCreateColor('Trans Flag Blue', '#5BCFFB'),
+	])
+}
+
+function createRainbowFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Pride Red', '#FF0018'),
+		getOrCreateColor('Pride Orange', '#FFA52C'),
+		getOrCreateColor('Pride Yellow', '#FFFF41'),
+		getOrCreateColor('Pride Green', '#008018'),
+		getOrCreateColor('Pride Blue', '#0000F9'),
+		getOrCreateColor('Pride Purple', '#86007D')
+	])
+}
+
+function createNonBinaryFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Non-Binary Yellow', '#FCF431'),
+		getOrCreateColor('Non-Binary White', '#FFFFFF'),
+		getOrCreateColor('Non-Binary Purple', '#9C59D1'),
+		getOrCreateColor('Non-Binary Black', '#000000')
+	])
+}
+
+function createPanFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Pansexual Pink', '#FF1B8D'),
+		getOrCreateColor('Pansexual Yellow', '#FFDA00'),
+		getOrCreateColor('Pansexual Blue', '#1BB3FF')
+	])
+}
+
+function createBisexualFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Bisexual Pink', '#D60270'),
+		getOrCreateColor('Bisexual Purple', '#9B4F96'),
+		getOrCreateColor('Bisexual Blue', '#0038A8')
+	])
+}
+
+function createAsexualFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Asexual Black', '#000000'),
+		getOrCreateColor('Asexual Grey', '#a4a4a4'),
+		getOrCreateColor('Asexual White', '#ffffff'),
+		getOrCreateColor('Asexual Purple', '#810081')
+	])
+}
+
+function createAromanticFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Aromantic Green 1', '#3ba740'),
+		getOrCreateColor('Aromantic Green 2', '#a8d47a'),
+		getOrCreateColor('Aromantic White', '#ffffff'),
+		getOrCreateColor('Aromantic Grey', '#ababab'),
+		getOrCreateColor('Aromantic Black', '#000000'),
+	])
+}
+
+function createLesbianFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Lesbian Red', '#D52D00'),
+		getOrCreateColor('Lesbian Orange', '#FF9A56'),
+		getOrCreateColor('Lesbian White', '#FFFFFF'),
+		getOrCreateColor('Lesbian Light Pink', '#D462A6'),
+		getOrCreateColor('Lesbian Pink', '#A50062'),
+	])
+}
+
+function createGenderfluidFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Genderfluid Pink', '#FE76A2'),
+		getOrCreateColor('Genderfluid White', '#FFFFFF'),
+		getOrCreateColor('Genderfluid Violet', '#BF12d7'),
+		getOrCreateColor('Genderfluid Black', '#000000'),
+		getOrCreateColor('Genderfluid Blue', '#303CBE')
+	]);
+}
+
+function createAgenderFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Agender Black', '#000'),
+		getOrCreateColor('Agender Gray', '#bababa'),
+		getOrCreateColor('Agender White', '#fff'),
+		getOrCreateColor('Agender Green', '#baf484'),
+		getOrCreateColor('Agender White', '#fff'),
+		getOrCreateColor('Agender Gray', '#bababa'),
+		getOrCreateColor('Agender Black', '#000'),
+	]);
+}
+
+function createGayMenFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Gay Men Green 1', '#068E70'),
+		getOrCreateColor('Gay Men Green 2', '#27CFAA'),
+		getOrCreateColor('Gay Men Green 3', '#98e9c1'),
+		getOrCreateColor('Gay Men White', '#FFFFFF'),
+		getOrCreateColor('Gay Men Blue 1', '#7bade2'),
+		getOrCreateColor('Gay Men Blue 2', "#5049cb"),
+		getOrCreateColor('Gay Men Blue 3', "#3c1a77"),
+	]);
+}
+
+function createGenderqueerFlag(newPage) {
+	createStripes(newPage, [
+		getOrCreateColor('Genderqueer Purple', "#b57fdd"),
+		getOrCreateColor('Genderqueer White', '#fff'),
+		getOrCreateColor('Genderqueer Green', "#49821e")
+	]);
+}
+
+/**
+ *
+ * @param {{parent: import('indesign').Page, bounds: [number, number, number, number]}} newPage
+ */
+function createText({ parent, bounds }) {
+	let textFrame = parent.textFrames.add();
+	textFrame.textFramePreferences.verticalJustification = VerticalJustification.CENTER_ALIGN;
+
+	textFrame.contents = getTextMessage() || '';
+	textFrame.paragraphs.firstItem().justification = Justification.CENTER_ALIGN;
+	textFrame.paragraphs.firstItem().pointSize = 32;
+	textFrame.paragraphs.firstItem().appliedFont = app.fonts.itemByName('Arial');
+
+	const padding = cbPadding.checked ? parent.marginPreferences.columnGutter : 0;
+	textFrame.geometricBounds = [
+		bounds[0] + bounds[2] / 5 * 2 + padding,
+		bounds[1] + parent.marginPreferences.left + padding,
+		bounds[2] / 5 * 3 - padding,
+		bounds[3] - parent.marginPreferences.right - padding
+	];
+	textFrame.fillColor = getOrCreateColor('White', [0, 0, 0, 0]);
+}
+
+function getOrCreateColor(name, colorValue) {
+	let color = app.activeDocument.colors.itemByName(name);
+	colorValue = toColorArray(colorValue);
+	if (!color.isValid) {
+		console.log(`Creating color ${name} because it doesn't exist`);
+		color = app.activeDocument.colors.add({
+			name: name,
+			space: colorValue.length > 3 ? ColorSpace.CMYK : ColorSpace.RGB,
+			colorValue: colorValue,
+			parentColorGroup: getOrCreateColorGroup()
+		});
+		color.space = ColorSpace.CMYK;
+	}
+	return color;
+}
+
+/**
+ * Converts a color to an InDesign color array
+ * @param {string | number[]} color The color to convert. Can be a hex string or an array of (3) RGB or (4) CMYK values
+ * @returns {number[]} The color as an array of either (3) RGB (for HEX and RGB input) or (4) CMYK (for CMYK input) values
+ * @throws {Error} If the color is not a valid color
+ */
+function toColorArray(color) {
+	if (Array.isArray(color))
+		return color;
+	if (typeof color !== 'string')
+		throw new Error(`Invalid color ${color}`);
+	if (color.startsWith('#'))
+		color = color.substring(1);
+	if (color.length === 3)
+		color = color.split('').map(c => c + c).join('');
+	if (color.length !== 6)
+		throw new Error(`Invalid color ${color}. Must be 3 or 6 characters long`);
+	return [
+		parseInt(color.substring(0, 2), 16),
+		parseInt(color.substring(2, 4), 16),
+		parseInt(color.substring(4, 6), 16)
+	];
+}
+
+function getOrCreateColorGroup() {
+	let colorGroup = app.activeDocument.colorGroups.itemByName('Pride Colors');
+	if (!colorGroup.isValid) {
+		console.log(`Creating color group Pride Colors because it doesn't exist`);
+		colorGroup = app.activeDocument.colorGroups.add('Pride Colors', undefined, {});
+	}
+	return colorGroup;
+}
